50fba5aa61324c2c6eb6e4b44ff815179ed26042
yumetodo <yume-wikijp@live.jp>
2016-11-12 15:16:26 +0900

    _mm_div_ps is 1%(+-2.4% N=19)faster than m128_rcp_22bit_ps
    avg	11352.5789473684	11233.5263157895
    sd	314.719811192093	307.312693217743
    sc	141.512685461962	138.182100227707
    sc.max	11494.0916328304	11371.7084160172
    sc.min	11211.0662619065	11095.3442155618
    	m128_rcp_22bit_ps	_mm_div_ps

--- SigColorFastAviUtl/sigmoid.hpp
+++ SigColorFastAviUtl/sigmoid.hpp
@@ -1,11 +1,6 @@
 ﻿#pragma once
 #include <cmath>
 #include <xmmintrin.h>
-inline __m128 m128_rcp_22bit_ps(const __m128& a) noexcept {
-	const __m128 xm1 = _mm_rcp_ps(a);//(1/a):11bit
-	const __m128 xm0 = _mm_mul_ps(_mm_mul_ps(a, xm1), xm1);//a * (1/a) * (1/a)
-	return _mm_sub_ps(_mm_add_ps(xm1, xm1), xm0);//(1/a) + (1/a) - a * (1/a) * (1/a)
-}
 inline float sigmoid(float a, float b, float u) noexcept {
 	//float e1, e2, e3, e4;
 	float result[4];
@@ -17,7 +12,7 @@ inline float sigmoid(float a, float b, float u) noexcept {
 	__m128 ei = _mm_loadu_ps(result);
 	static const __m128 v1 = _mm_set1_ps(1.0f);
 	const __m128 dst = _mm_add_ps(ei, v1);
-	ei = m128_rcp_22bit_ps(dst);
+	ei = _mm_div_ps(v1, dst);
 
 	_mm_storeu_ps(result, ei);
 	return (result[0] - result[1]) / (result[2] - result[3]);
