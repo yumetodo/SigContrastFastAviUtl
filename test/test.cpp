2c44c6a5c0554cd030d6c7f255ada660efbbc8b6
yumetodo <yume-wikijp@live.jp>
2016-11-12 18:56:40 +0900

    fix testcase

--- test/test.cpp
+++ test/test.cpp
@@ -12,8 +12,10 @@ IUTEST_TEST(SigmoidTableCompatibility, SigmoidTable_test) {
 			old::SigmoidTable old_table(m / 100.0f, static_cast<float>(s), 4096, 4096.0);
 			new_table.change_param(m / 100.0f, static_cast<float>(s));
 			for (int i = 0; i <= 4096; ++i) {
-				IUTEST_ASSERT_NEAR(static_cast<float>(old_table.lookup(i)), static_cast<float>(new_table.lookup(i)), 1.0f)
+				IUTEST_ASSERT(0 <= new_table.lookup(i) && new_table.lookup(i) <= 4096);
+				IUTEST_EXPECT(old_table.lookup(i) == new_table.lookup(i))
 					<< " (when i=" << i << " m=" << m << " s=" << s << ')';
+				IUTEST_ASSERT_NEAR(static_cast<float>(old_table.lookup(i)), static_cast<float>(new_table.lookup(i)), 1.0f);
 			}
 		}
 	}
@@ -21,13 +23,16 @@ IUTEST_TEST(SigmoidTableCompatibility, SigmoidTable_test) {
 IUTEST_TEST(SigmoidTableCompatibility, RSigmoidTable_test) {
 	//0.0 <= midtone <= 1.0, 1.0 <= strength <= 30.0
 	RSigmoidTable new_table;
+	new_table.change_param(1.0f, 30.0f);
 	for (int m = 0; m <= 100; ++m) {
 		for (int s = 1; s <= 30; ++s) {
 			old::RSigmoidTable old_table(m / 100.0f, static_cast<float>(s), 4096, 4096.0);
 			new_table.change_param(m / 100.0f, static_cast<float>(s));
 			for (int i = 0; i <= 4096; ++i) {
-				IUTEST_EXPECT_NEAR(static_cast<float>(old_table.lookup(i)), static_cast<float>(new_table.lookup(i)), 2.0f);
-				IUTEST_ASSERT_NEAR(static_cast<float>(old_table.lookup(i)), static_cast<float>(new_table.lookup(i)), 3.0f);
+				IUTEST_ASSERT(0 <= new_table.lookup(i) && new_table.lookup(i) <= 4096);
+				IUTEST_EXPECT(old_table.lookup(i) == new_table.lookup(i))
+					<< " (when i=" << i << " m=" << m << " s=" << s << ')';
+				IUTEST_ASSERT_NEAR(static_cast<float>(old_table.lookup(i)), static_cast<float>(new_table.lookup(i)), 1.0f);
 			}
 		}
 	}
