de6f72158c20d4acb9395ed6d0cbf5c936bdbfc2
yumetodo <yume-wikijp@live.jp>
2016-11-14 03:24:48 +0900

    add std_future::size

---
+++ SigColorFastAviUtl/std_future.hpp
@@ -0,0 +1,8 @@
+﻿#pragma once
+#include <cstddef>
+namespace std_future{
+	template <class C> 
+	constexpr auto size(const C& c) -> decltype(c.size()) { return c.size(); }
+	template <class T, std::size_t N>
+	constexpr std::size_t size(const T (&)[N]) noexcept { return N; }
+}
