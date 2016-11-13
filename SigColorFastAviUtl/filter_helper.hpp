0f061f58e3703d08fb14d020d7adfb37d546bba7
yumetodo <yume-wikijp@live.jp>
2016-11-14 03:25:58 +0900

    fillter_proxy: add ostream operator

--- SigColorFastAviUtl/filter_helper.hpp
+++ SigColorFastAviUtl/filter_helper.hpp
@@ -1,6 +1,9 @@
 ﻿#pragma once
 #include <cstddef>
 #include <cstdint>
+#ifdef USECLOCK
+#	include <iostream>
+#endif
 #include "filter.h"
 enum class track : std::uint8_t {
 	midtone = 0,
@@ -32,12 +35,12 @@ enum filter_update_check_status : int {
 };
 class filter_proxy {
 	filter_proxy() = delete;
-	filter_proxy(filter_proxy&&) = delete;
 	filter_proxy& operator=(const filter_proxy&) = delete;
 	filter_proxy& operator=(filter_proxy&&) = delete;
 	FILTER* fp_;
 public:
 	filter_proxy(const filter_proxy&) = default;
+	filter_proxy(filter_proxy&&) = default;
 	explicit filter_proxy(FILTER* fp) : fp_(fp) {}
 	bool any_of(check c) const noexcept { return 0 != this->fp_->check[static_cast<std::size_t>(c)]; }
 	template<typename T, typename... Rest, std::enable_if_t<std::is_same<T, check>::value, std::nullptr_t> = nullptr>
@@ -50,11 +53,26 @@ public:
 	int& operator[](check c) noexcept { return this->fp_->check[static_cast<std::size_t>(c)]; }
 	void set_rgb(bool state) noexcept { (*this)[check::R] = (*this)[check::G] = (*this)[check::B] = state; }
 	bool notify_update_window() const noexcept { return 0 != this->fp_->exfunc->filter_window_update(this->fp_); }
+#ifdef USECLOCK
 	void disable_benchmark() noexcept {
 		if ((*this)[check::disable_benchmark_during_export]) {
 			(*this)[check::echo_benchmark] = (*this)[check::save_benchmark] = false;
 			this->notify_update_window();
 		}
 	}
+#endif
 	HWND window_handle() const { return this->fp_->hwnd; }
 };
+#ifdef USECLOCK
+inline std::ostream& operator<<(std::ostream& os, const filter_proxy& fc) {
+	static constexpr const char* y[] = { "", "Y" };
+	static constexpr const char* r[] = { "", "R" };
+	static constexpr const char* g[] = { "", "G" };
+	static constexpr const char* b[] = { "", "B" };
+	os
+		<< "convert mode," << y[fc[check::Y]] << r[fc[check::R]] << g[fc[check::G]] << b[fc[check::B]] << std::endl
+		<< "midtone," << fc[track::midtone] << std::endl
+		<< "strength," << fc[track::strength] << std::endl;
+	return os;
+}
+#endif
