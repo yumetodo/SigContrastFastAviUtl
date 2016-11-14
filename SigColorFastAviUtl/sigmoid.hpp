1a3925c89014786ae966d5ef508554d306854511
yumetodo <yume-wikijp@live.jp>
2016-11-14 13:10:54 +0900

    sigmoid: add overload to pre calcurate non-dependent with input part

--- SigColorFastAviUtl/sigmoid.hpp
+++ SigColorFastAviUtl/sigmoid.hpp
@@ -1,6 +1,19 @@
 ﻿#pragma once
 #include <cmath>
 #include <xmmintrin.h>
+namespace detail {
+	inline float sigmoid_impl(__m128 ei) noexcept {
+		//float e1, e2, e3, e4;
+		float result[4];
+		static const __m128 v1 = _mm_set1_ps(1.0f);
+		const __m128 dst = _mm_add_ps(ei, v1);
+		ei = _mm_div_ps(v1, dst);
+
+		_mm_storeu_ps(result, ei);
+		return (result[0] - result[1]) / (result[2] - result[3]);
+	}
+
+}
 inline float sigmoid(float a, float b, float u) noexcept {
 	//float e1, e2, e3, e4;
 	float result[4];
@@ -9,11 +22,19 @@ inline float sigmoid(float a, float b, float u) noexcept {
 	result[2] = std::expf(b*(a - 1));
 	result[3] = result[1];
 
-	__m128 ei = _mm_loadu_ps(result);
-	static const __m128 v1 = _mm_set1_ps(1.0f);
-	const __m128 dst = _mm_add_ps(ei, v1);
-	ei = _mm_div_ps(v1, dst);
+	return detail::sigmoid_impl(_mm_loadu_ps(result));
+}
+inline float sigmoid(float a, float b, float u, __m128 pre) noexcept {
+	//float e1, e2, e3, e4;
+	const __m128 tmp = _mm_set_ss(std::expf(b*(a - u)));
+	return detail::sigmoid_impl(_mm_or_ps(pre, tmp));
+}
+inline __m128 sigmoid_pre(float a, float b) noexcept {
+	float result[4];
+	result[0] = 0.0f;
+	result[1] = std::expf(a*b);
+	result[2] = std::expf(b*(a - 1));
+	result[3] = result[1];
 
-	_mm_storeu_ps(result, ei);
-	return (result[0] - result[1]) / (result[2] - result[3]);
+	return _mm_loadu_ps(result);
 }
