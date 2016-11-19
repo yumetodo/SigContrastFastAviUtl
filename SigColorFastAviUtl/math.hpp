a139a0e77329e358554310a30f611624ac1824ab
yumetodo <yume-wikijp@live.jp>
2016-11-19 17:33:03 +0900

    divide abs_diff to another file

---
+++ SigColorFastAviUtl/math.hpp
@@ -0,0 +1,107 @@
+﻿#pragma once
+#include <limits>
+#include <type_traits>
+#include <stdexcept>
+namespace math {
+	namespace detail {
+		template<typename T1, typename T2>
+		struct max_type : std::conditional<(sizeof(T1) < sizeof(T2)), T2, T1> {};
+		template<typename T1, typename T2>
+		using max_type_t = typename max_type<T1, T2>::type;
+		template<typename T1, typename T2, std::enable_if_t<sizeof(T1) == sizeof(T2), std::nullptr_t> = nullptr>
+		constexpr bool abs_diff_can_noexcept() {
+			return std::numeric_limits<max_type_t<T1, T2>>::min() + 1 == -std::numeric_limits<max_type_t<T1, T2>>::max()
+				|| std::numeric_limits<max_type_t<T1, T2>>::min() == -std::numeric_limits<max_type_t<T1, T2>>::max();
+		}
+		template<typename T1, typename T2, std::enable_if_t<sizeof(T1) != sizeof(T2), std::nullptr_t> = nullptr>
+		constexpr bool abs_diff_can_noexcept() {
+			return std::numeric_limits<max_type_t<T1, T2>>::min() + 1 == -std::numeric_limits<max_type_t<T1, T2>>::max()
+				|| std::numeric_limits<max_type_t<T1, T2>>::min() == -std::numeric_limits<max_type_t<T1, T2>>::max();
+		}
+		/**
+		* @param a bigger unsigned num
+		* @param b smaller signed negative num
+		*/
+		template <typename T1, typename T2, std::enable_if_t<
+			std::is_unsigned<T1>::value && std::is_signed<T2>::value,
+			std::nullptr_t
+		> = nullptr>
+		static inline auto abs_diff_impl(const T1& a, const T2& b) noexcept(detail::abs_diff_can_noexcept<T1, T2>())
+			->std::make_unsigned_t<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>
+		{
+			using lim = std::numeric_limits<T2>;
+			using utype = std::make_unsigned_t<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>;
+			using ulim = std::numeric_limits<utype>;
+			//prevent overflow
+			//http://qiita.com/a4lg/items/bc4d2cfbce22fe749589
+			//-std::numeric_limits<T>::min() < std::numeric_limits<T>::max() : iregal after C99
+			//std::numeric_limits<T>::min() < -std::numeric_limits<T>::max() : most familiar behavior
+			//std::numeric_limits<T>::min() = -std::numeric_limits<T>::max() : possible
+			//note: a is positive
+			return (-lim::max() <= b)
+				//``-b`` is no problem
+				? static_cast<utype>(a) + static_cast<std::make_unsigned_t<T2>>(-b)
+				//std::numeric_limits<T>::min() <= b < -std::numeric_limits<T>::max()
+				: 
+				(
+					(lim::max() < (ulim::max() - static_cast<utype>(a)))
+					//	(try to store rest) (----------------storable max num----------------)
+					&& ((b - lim::min()) <= (ulim::max() - lim::max() - static_cast<utype>(a)))
+				)
+					//can store
+					? static_cast<utype>(a) + static_cast<utype>(lim::max()) + static_cast<utype>((-lim::max()) - b)
+					//when processing system doesn't use two's complement and 
+					//std::numeric_limits<T>::min() < -std::numeric_limits<T>::max()
+					//there is possibility no way to store result.
+					//In that case, we throw exception.
+					: throw std::invalid_argument();
+		}
+	}
+	template <typename T1, typename T2, std::enable_if_t<
+		std::is_signed<T1>::value && std::is_signed<T2>::value,
+		std::nullptr_t
+	> = nullptr>
+	static inline constexpr auto abs_diff(const T1& a, const T2& b) noexcept(detail::abs_diff_can_noexcept<T1, T2>::value)
+		->std::make_unsigned_t<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>
+	{
+		using lim = std::numeric_limits<T2>;
+		using utype = std::make_unsigned_t<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>;
+		using ulim = std::numeric_limits<utype>;
+		return (b < a)
+			? abs_diff(a, b)
+			: (b <= 0 || lim::min() + b <= a)
+				? static_cast<utype>(b - a)
+				: detail::abs_diff_impl(static_cast<utype>(b), a);
+	}
+	template <typename T1, typename T2, std::enable_if_t<
+		std::is_unsigned<T1>::value && std::is_signed<T2>::value,
+		std::nullptr_t
+	> = nullptr>
+	static inline constexpr auto abs_diff(const T1& a, const T2& b) noexcept
+		->std::conditional_t<(sizeof(T1) < sizeof(T2)), std::make_unsigned_t<T2>, T1>
+	{
+		return (a < b)
+			? static_cast<std::make_unsigned_t<T2>>(b) - a
+			: (0 <= b)
+				? a - static_cast<std::make_unsigned_t<T2>>(b)
+				: detail::abs_diff_impl(a, b);
+	}
+	template <typename T1, typename T2, std::enable_if_t<
+		std::is_signed<T1>::value && std::is_unsigned<T2>::value,
+		std::nullptr_t
+	> = nullptr>
+	static inline constexpr auto abs_diff(const T1& a, const T2& b) noexcept
+		->std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, std::make_unsigned_t<T1>>
+	{
+		return abs_diff(b, a);
+	}
+	template <typename T1, typename T2, std::enable_if_t<
+		std::is_unsigned<T1>::value && std::is_unsigned<T2>::value,
+		std::nullptr_t
+	> = nullptr>
+	static inline constexpr auto abs_diff(const T1& a, const T2& b) noexcept
+		->std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>
+	{
+		return (a < b) ? b - a : a - b;
+	}
+}
--- SigColorFastAviUtl/thread.hpp
+++ SigColorFastAviUtl/thread.hpp
@@ -2,86 +2,10 @@
 #include <thread>
 #include <type_traits>
 #include <vector>
-#include <limits>
 #include <future>
 #include <iterator>
+#include "math.hpp"
 namespace parallel {
-	namespace detail {
-		template <typename T1, typename T2, std::enable_if_t<
-			std::is_signed<T1>::value && std::is_signed<T2>::value,
-			std::nullptr_t
-		> = nullptr>
-		static inline constexpr auto abs_diff(const T1& a, const T2& b) 
-			noexcept(
-				(
-					std::numeric_limits<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>::min() + 1
-					== -std::numeric_limits<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>::max()
-				) || (
-					std::numeric_limits<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>::min()
-					== -std::numeric_limits<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>::max()
-				)
-			)
-			-> std::make_unsigned_t<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>
-		{
-			using lim = std::numeric_limits<T2>;
-			using utype = std::make_unsigned_t<std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>>;
-			using ulim = std::numeric_limits<utype>;
-			return (b < a) 
-				? abs_diff(a, b)
-				: (b <= 0 || lim::min() + b <= a)
-					? static_cast<utype>(b - a)
-					//prevent overflow
-					//http://qiita.com/a4lg/items/bc4d2cfbce22fe749589
-					//-std::numeric_limits<T>::min() < std::numeric_limits<T>::max() : iregal after C99
-					//std::numeric_limits<T>::min() < -std::numeric_limits<T>::max() : most familiar behavior
-					//std::numeric_limits<T>::min() = -std::numeric_limits<T>::max() : possible
-					//note: b is positive
-					: (-lim::max() <= a)
-						//``-a`` is no problem
-						? static_cast<utype>(b) + static_cast<std::make_unsigned_t<T2>>(-a)
-						//std::numeric_limits<T>::min() <= a < -std::numeric_limits<T>::max()
-						//(try to store rest)  (----------------storable max num----------------)
-						: ((a - lim::min()) <= (ulim::max() - lim::max() - static_cast<utype>(b)))
-							//can store
-							? static_cast<utype>(b) + static_cast<utype>(lim::max()) + static_cast<utype>((-lim::max()) - a)
-							//when processing system doesn't use two's complement and 
-							//std::numeric_limits<T>::min() < -std::numeric_limits<T>::max()
-							//there is possibility no way to store result.
-							//In that case, we throw exception.
-							: throw std::invalid_argument();
-		}
-		template <typename T1, typename T2, std::enable_if_t<
-			std::is_unsigned<T1>::value && std::is_signed<T2>::value,
-			std::nullptr_t
-		> = nullptr>
-		static inline constexpr auto abs_diff(const T1& a, const T2& b) noexcept
-			->std::conditional_t<(sizeof(T1) < sizeof(T2)), std::make_unsigned_t<T2>, T1>
-		{
-			return (a < b)
-				? static_cast<std::make_unsigned_t<T2>>(b) - a
-				: (b < 0)
-					? a + static_cast<std::make_unsigned_t<T2>>(-b)
-					: a - static_cast<std::make_unsigned_t<T2>>(b);
-		}
-		template <typename T1, typename T2, std::enable_if_t<
-			std::is_signed<T1>::value && std::is_unsigned<T2>::value,
-			std::nullptr_t
-		> = nullptr>
-		static inline constexpr auto abs_diff(const T1& a, const T2& b) noexcept
-			->std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, std::make_unsigned_t<T1>>
-		{
-			return abs_diff(b, a);
-		}
-		template <typename T1, typename T2, std::enable_if_t<
-			std::is_unsigned<T1>::value && std::is_unsigned<T2>::value,
-			std::nullptr_t
-		> = nullptr>
-		static inline constexpr auto abs_diff(const T1& a, const T2& b) noexcept
-			->std::conditional_t<(sizeof(T1) < sizeof(T2)), T2, T1>
-		{
-			return (a < b) ? b - a : a - b;
-		}
-	}
 	template<
 		typename Index, typename Func,
 		typename ...Args,
@@ -93,7 +17,7 @@ namespace parallel {
 			f(begin, end, std::forward<Args>(args)...);
 		}
 		else {
-			const auto num = parallel::detail::abs_diff(end, begin);
+			const auto num = math::abs_diff(end, begin);
 			const auto task_num = num / thread_num;
 			const auto task_rest = num % thread_num;
 			std::vector<std::thread> th;
@@ -186,7 +110,7 @@ namespace parallel {
 			re.push_back(std::async(std::launch::deferred, f, begin, end, std::forward<Args>(args)...));
 		}
 		else {
-			const auto num = parallel::detail::abs_diff(end, begin);
+			const auto num = math::abs_diff(end, begin);
 			const auto task_num = num / thread_num;
 			const auto task_rest = num % thread_num;
 			re.reserve(thread_num);
