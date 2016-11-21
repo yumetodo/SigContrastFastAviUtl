528eed4bf39b9115a0d064e7b0253a28ce108330
yumetodo <yume-wikijp@live.jp>
2016-11-21 17:18:35 +0900

    I was forgot to bump version

--- SigColorFastAviUtl/version.h
+++ SigColorFastAviUtl/version.h
@@ -1,7 +1,7 @@
 #ifndef SIG_VERSION_H_
 #define SIG_VERSION_H_
-#define SIG_COLOR_VERSION  0,3,0
-#define SIG_COLOR_VERSION_STR "0.3.0"
+#define SIG_COLOR_VERSION  1,0,0
+#define SIG_COLOR_VERSION_STR "1.0.0"
 #ifdef _DEBUG
 #   define SIG_COLOR_AUF_INFO         "SigColorFastAviUtl(DEBUG)"
 #else
